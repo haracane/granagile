d1da5cb3667af351c809625048fbc977f86d46e8
Kenji Hara <haracane@gmail.com>
2013-02-25 09:41:57 +0900

    Fixed alminium vcs auto create recipe

--- chef-repo/site-cookbooks/centos/recipes/auto-create-alminium-vcs.rb
+++ chef-repo/site-cookbooks/centos/recipes/auto-create-alminium-vcs.rb
@@ -1,19 +1,4 @@
 [
-  "opt/granagile/script/create-alminium-git.sh",
-  "opt/granagile/script/create-alminium-svn.sh"
-  ].each do |filename|
-  filepath = "/#{filename}"
-  template filepath do
-    source "#{filename}.erb"
-  end
-  file filepath do
-    owner "root"
-    group "root"
-    mode  "0755"
-  end
-end
-
-[
   "etc/cron.d/create-alminium-vcs"
   ].each do |filename|
   filepath = "/#{filename}"
